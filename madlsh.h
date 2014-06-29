022526ca9951d2566127e69bd2a4d2edcace4fb2
Nicolas Koumchatzky <nkoumchatzky@users.noreply.github.com>
2014-06-29 16:53:43 +0200

    Added comments.

--- madlsh.h
+++ madlsh.h
@@ -7,6 +7,11 @@
 
 using namespace std;
 
+/*
+ * ThresholdIndex Structure : for every hash function h of every amplification function j,
+ * choose a random dimension i and an input threshold T from the real vector structure.
+ * If the value of the input data for the dimension i is over the threshold T, then the hash function returns 1, and 0 otherwise.
+ */
 typedef struct
 {
 	//Number of amplification functions
@@ -37,8 +42,10 @@ typedef struct
     //LxM array storing the threshold that have been randomly selected.
     float** threshold_array;
 
+    //Local char array used to speed up hashing.
     char*	local_char_array;
 
+    //Local int vector used to speed up hashing.
     vector<int>	hash_table;
 
 
@@ -46,10 +53,27 @@ typedef struct
 
 
 extern "C" {
+	/*
+	 * Initialize the ThresholdIndex with the number of amplification (aggregation) functions l,
+	 * the number of hash functions per amplification function m,
+	 * the dimension of the input data data_dim,
+	 * the array of minimum values for every dimension min_vect,
+	 * the array of maximum values for every dimension max_vect.
+	 * init(...) chooses randomly m dimension for every amplification function,
+	 * and randomly selects a threshold between min_vect[i] and max_vect[i] for every chosen dimension i.
+	 */
 	ThresholdIndex *init(int l, int m, int data_dim, float* min_vect, float* max_vect);
+
+	/*
+	 * Insert the array data in the index (computes a hash table for every amplification function).
+	 */
+    void insert(ThresholdIndex *index, float* data);
+
+	/*
+	 *
+	 */
     float** query(ThresholdIndex *index, float* data, int N, int* res_number);
     void add_to_group(ThresholdIndex *index, float* data, int group);
-    void insert(ThresholdIndex *index, float* data);
     int hash_value(ThresholdIndex *index, float* data, int i, int j);
     void hash_key(ThresholdIndex *index, string& hk, float* data, int group);
 //    char* hash_key(ThresholdIndex *index, float* data, int group);
